c5e6bc98e2aca5ea34888b333414431a78cff3da
shahriarahmadf <shahriarahmadf@gmail.com>
2023-03-14 16:41:35 +0900

    integrated mongodb

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express= require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,12 +10,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// database access
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.syljfiu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 // READ
 app.get('/', (req,res) => {
     res.send('genius car server running');
-})
+});
 
 // 
-app.listen(post, () => {
+app.listen(port, () => {
     console.log(`Genius car server running on ${port}`);
-})
+});
